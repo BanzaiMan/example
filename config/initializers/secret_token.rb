170598014263f55027d032737a0f3f90eb661d44
Hiro Asari <asari.ruby@gmail.com>
2013-06-25 20:54:58 -0400

    Template Rails 4 app with PostgreSQL

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Example::Application.config.secret_key_base = 'a60d4c78e5a6b025a3459002d59516bf9a9d67c361fe6e5703ed769ba159a025a06845297d9be3f594ed13c2f4ca3dfd796118cb5116b0e6f70c0c381beb01db'
